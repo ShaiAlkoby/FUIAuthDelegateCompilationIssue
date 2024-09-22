339239f94b10b74a276507635ab0a54dc0d08e96
shaialkoby <alkoby.shai@gmail.com>
2024-09-22 09:14:10 +0300

    initial commit

---
+++ FUIAuthDelegateCompilationIssueTests/FUIAuthDelegateCompilationIssueTests.swift
@@ -0,0 +1,17 @@
+//
+//  FUIAuthDelegateCompilationIssueTests.swift
+//  FUIAuthDelegateCompilationIssueTests
+//
+//  Created by Shai Alkoby on 22/09/2024.
+//
+
+import Testing
+@testable import FUIAuthDelegateCompilationIssue
+
+struct FUIAuthDelegateCompilationIssueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
